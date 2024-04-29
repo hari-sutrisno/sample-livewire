8644de734c07b9ac4c2e4efd7502d0c565957780
hari-sutrisno <hrstr93@gmail.com>
2024-04-29 20:28:28 +0700

    Membuat title yang dinamis

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Users\Show;
 use Illuminate\Support\Facades\Route;
 
 /* Route::get('/', function () {
@@ -9,3 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', \App\Livewire\Home::class)->name('home');
 Route::get('/about', \App\Livewire\About::class)->name('about');
+Route::get('/contact', \App\Livewire\Contact::class)->name('contact');
+Route::get('/posts', \App\Livewire\Posts\Index::class)->name('posts.index');
+
+Route::get('users/{user}', \App\Livewire\Users\Show::class)->name('users.show');
